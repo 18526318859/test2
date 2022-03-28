6c7161475abf01ebcfef0f61f2bc406c4f83b819
root <2864483641@qq.com>
2022-03-28 11:08:05 +0800

    仓库缓冲测试

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+
+
+    @Test
+    public void contextLoads()  {
+
+    }
+
+
+}
